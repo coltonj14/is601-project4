0b264969887df5b606e537041712e3de27e2e4bd
Colton Johnson <cj236@njit.edu>
2022-05-11 00:56:14 -0400

    update conftest and add config tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,50 @@
-# project/tests/test_config.py
+"""This makes the test configuration setup"""
+# pylint: disable=redefined-outer-name
 import logging
 import os
 
-import app.config
+import pytest
+from app import create_app, User
+from app.db import db
+
+#this is a good tutorial I used to fix this code to do datbase testing.
+#https://xvrdm.github.io/2017/07/03/testing-flask-sqlalchemy-database-with-pytest/
+
+@pytest.fixture()
+def application():
+    """This makes the app"""
+    #you need this one if you want to see whats in the database
+    #os.environ['FLASK_ENV'] = 'development'
+    #you need to run it in testing to pass on github
+    os.environ['FLASK_ENV'] = 'testing'
+
+    application = create_app()
+
+    with application.app_context():
+        db.create_all()
+        yield application
+        db.session.remove()
+        #drops the database tables after the test runs
+        #db.drop_all()
+
+@pytest.fixture()
+def add_user(application):
+    with application.app_context():
+        #new record
+        user = User('keith@webizly.com', 'testtest')
+        db.session.add(user)
+        db.session.commit()
 
 
-def test_development_config(application):
-    application.config.from_object('app.config.DevelopmentConfig')
 
-    assert application.config['DEBUG']
-    assert not application.config['TESTING']
 
-def test_testing_config(application):
-    application.config.from_object('app.config.TestingConfig')
-    assert application.config['DEBUG']
-    assert application.config['TESTING']
-    assert not application.config['PRESERVE_CONTEXT_ON_EXCEPTION']
-    assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:///:memory:'
+@pytest.fixture()
+def client(application):
+    """This makes the http client"""
+    return application.test_client()
 
-def test_production_config(application):
-    application.config.from_object('app.config.ProductionConfig')
-    assert not application.config['DEBUG']
-    assert not application.config['TESTING']
+
+@pytest.fixture()
+def runner(application):
+    """This makes the task runner"""
+    return application.test_cli_runner()
