29d2f8674b161d477fc42e367fb1b84f1a991dfd
Colton Johnson <cj236@njit.edu>
2022-05-11 00:49:55 -0400

    Add test dashboard access and authenticated user test

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -44,3 +44,16 @@ def test_registration(client):
         print(res.data)
         assert res.status_code == 200
         assert b'href="/login"' in res.data
+
+
+def test_dashboard_access(client):
+    """Test dashboard access"""
+    res = client.get("/dashboard")
+    assert res.status_code == 302
+
+
+
+def test_authenticated_user(client):
+    """Test user authentication"""
+    user = User("cj236@njit.edu", "testtest")
+    assert user.is_authenticated() == True
