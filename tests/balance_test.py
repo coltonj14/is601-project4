9d866ee70a042ba4ee7cb30b396de6fe1dc90ef7
Colton Johnson <cj236@njit.edu>
2022-05-11 16:27:47 -0400

    Add Balance tests

---
+++ tests/balance_test.py
@@ -0,0 +1,30 @@
+"""Test the balance"""
+from app import db
+from app.db.models import User, Transactions
+
+
+
+def test_balance_none(application, client):
+    """Test if a user logs in that it displays balance"""
+    with application.app_context():
+        # Add user to be able to test login
+        user = User('cj236@njit.edu', 'hellotest')
+        db.session.add(user)
+        db.session.commit()
+
+        print(user.balance)
+        assert user.balance == None
+
+
+
+def test_balance_calculations(application, client):
+    """Test the balance calculations of the app"""
+    testdata = [2000, 2000, -100, 200, 200, 200, -1000, 200, 1000, -100, 100, 1000, 1000, -100, 1000, 100
+                -100, 100, 100, 100, -100, 100, 4242, -2324, 4242, 432, 432, -4323]
+    balance = 0
+    for data in testdata:
+        balance += data
+
+    assert balance == 10601
+
+
