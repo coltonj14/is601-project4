e15474ef02e803eb4f8a9d877af37c416785f405
Colton Johnson <cj236@njit.edu>
2022-05-11 14:10:15 -0400

    Add test request index

--- tests/simple_pages_tests.py
+++ tests/simple_pages_tests.py
@@ -8,3 +8,12 @@ def test_request_main_menu_links(client):
     assert b'href="/welcome"' in response.data
     assert b'href="/login"' in response.data
     assert b'href="/register"' in response.data
+
+
+def test_request_index(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Index" in response.data
+
+
