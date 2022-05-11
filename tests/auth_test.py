1871b2b135f4c6cd09dbf1b6d6c63edb330abd49
Colton Johnson <cj236@njit.edu>
2022-05-11 00:47:22 -0400

    Add auth test file and main menu test

---
+++ tests/auth_test.py
@@ -0,0 +1,11 @@
+"""This test the homepage"""
+from app import db
+from app.db.models import User, Transactions
+
+def test_request_main_menu_links(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b'href="/login"' in response.data
+    assert b'href="/register"' in response.data
+
