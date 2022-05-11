ef694dcf201abc23ee31eb6a97e5df856fa1d985
Colton Johnson <cj236@njit.edu>
2022-05-11 00:48:31 -0400

    Add login test

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -17,3 +17,21 @@ def test_auth_pages(client):
     assert response.status_code == 200
     response = client.get("/login")
     assert response.status_code == 200
+
+def test_login(application, client):
+    """Test that a user login functionality works"""
+    with application.app_context():
+        #Add user to be able to test login
+        user = User('cj236@njit.edu', 'testtest')
+        db.session.add(user)
+        db.session.commit()
+
+        res = client.post('/login', data=dict(email="cj236@njit.edu", password='testtest'), follow_redirects=True)
+        assert res.status_code == 200
+        assert b"Welcome" in res.data
+
+        #Test that the user can navigate to the dashboard and that it displays the user
+        dres = client.get("/dashboard", follow_redirects=True)
+        assert b"cj236" in dres.data
+
+        db.session.delete(user)
